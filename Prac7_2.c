d3f87747429ec442fa70fdf709f2442d6dfab0b0
Khushi <25it473@bvmengineering.ac.in>
2025-10-22 23:30:41 +0530

    Sum of first N number using user defined function

--- Prac7_2.c
+++ Prac7_2.c
@@ -1,1 +1,21 @@
 //Program to add first N number using function with argument and return type
+#include <stdio.h>
+
+int addNumbers(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main() {
+    int N;
+    printf("Enter a positive integer: ");
+    scanf("%d", &N);
+    
+    int result = addNumbers(N);
+    printf("The sum of the first %d natural numbers is: %d\n", N, result);
+    
+    return 0;
+}
